f64ccb74fee0d944d9bc6b0fe9aed792239c1aeb
Javier Honduvilla Coto <javierhonduco@gmail.com>
2016-12-10 22:42:04 +0100

    style

--- mygdb/mygdb.h
+++ mygdb/mygdb.h
@@ -1,34 +1,34 @@
 enum debugging_state {
-	SINGLE_STEP,
-	NON_STOP	
+  SINGLE_STEP,
+  NON_STOP
 };
 
 void show_registers(struct user_regs_struct *regs){
-	printf("%s=%lli ", "r15", regs->r15);
-	printf("%s=%lli ", "r14", regs->r14);
-	printf("%s=%lli ", "r13", regs->r13);
-	printf("%s=%lli ", "r12", regs->r12);
-	printf("%s=%lli ", "rbp", regs->rbp);
-	printf("%s=%lli ", "rbx", regs->rbx);
-	printf("%s=%lli ", "r11", regs->r11);
-	printf("%s=%lli ", "r10", regs->r10);
-	printf("%s=%lli ", "r9", regs->r9);
-	printf("%s=%lli ", "r8", regs->r8);
-	printf("%s=%lli ", "rax", regs->rax);
-	printf("%s=%lli ", "rcx", regs->rcx);
-	printf("%s=%lli ", "rdx", regs->rdx);
-	printf("%s=%lli ", "rsi", regs->rsi);
-	printf("%s=%lli ", "orig_rax", regs->orig_rax);
-	printf("%s=%lli ", "rip", regs->rip);
-	printf("%s=%lli ", "cs", regs->cs);
-	printf("%s=%lli ", "eflags", regs->eflags);
-	printf("%s=%lli ", "rsp", regs->rsp);
-	printf("%s=%lli ", "ss", regs->ss);
-	printf("%s=%lli ", "fs_base", regs->fs_base);
-	printf("%s=%lli ", "gs_base", regs->gs_base);
-	printf("%s=%lli ", "ds", regs->ds);
-	printf("%s=%lli ", "es", regs->es);
-	printf("%s=%lli ", "fs", regs->fs);
-	printf("%s=%lli ", "gs", regs->gs);
-	printf("\n");
+  printf("%s=%lli ", "r15", regs->r15);
+  printf("%s=%lli ", "r14", regs->r14);
+  printf("%s=%lli ", "r13", regs->r13);
+  printf("%s=%lli ", "r12", regs->r12);
+  printf("%s=%lli ", "rbp", regs->rbp);
+  printf("%s=%lli ", "rbx", regs->rbx);
+  printf("%s=%lli ", "r11", regs->r11);
+  printf("%s=%lli ", "r10", regs->r10);
+  printf("%s=%lli ", "r9", regs->r9);
+  printf("%s=%lli ", "r8", regs->r8);
+  printf("%s=%lli ", "rax", regs->rax);
+  printf("%s=%lli ", "rcx", regs->rcx);
+  printf("%s=%lli ", "rdx", regs->rdx);
+  printf("%s=%lli ", "rsi", regs->rsi);
+  printf("%s=%lli ", "orig_rax", regs->orig_rax);
+  printf("%s=%lli ", "rip", regs->rip);
+  printf("%s=%lli ", "cs", regs->cs);
+  printf("%s=%lli ", "eflags", regs->eflags);
+  printf("%s=%lli ", "rsp", regs->rsp);
+  printf("%s=%lli ", "ss", regs->ss);
+  printf("%s=%lli ", "fs_base", regs->fs_base);
+  printf("%s=%lli ", "gs_base", regs->gs_base);
+  printf("%s=%lli ", "ds", regs->ds);
+  printf("%s=%lli ", "es", regs->es);
+  printf("%s=%lli ", "fs", regs->fs);
+  printf("%s=%lli ", "gs", regs->gs);
+  printf("\n");
 }
--- mystrace/mystrace.c
+++ mystrace/mystrace.c
@@ -10,98 +10,98 @@
 int child_status;
 
 void die(char* message){
-	printf("failed with message: '%s'\n", message);
-	exit(-1);
+  printf("failed with message: '%s'\n", message);
+  exit(-1);
 }
 
 void tracee(char* cmd[]){
-	printf("I'm the tracee with pid=%d\n", getpid());
-	if(ptrace(PTRACE_TRACEME, NULL, NULL, NULL)<0){
-		die("error traceing myself");
-	}
-	execvp(cmd[0], cmd);
+  printf("I'm the tracee with pid=%d\n", getpid());
+  if(ptrace(PTRACE_TRACEME, NULL, NULL, NULL)<0){
+    die("error traceing myself");
+  }
+  execvp(cmd[0], cmd);
 }
 
 void tracee_with_pid(int pid){
-	// todo: detach later
-	if(ptrace(PTRACE_ATTACH, pid, NULL, NULL)<0){
-		die("error attaching to a pid\n does the pid exist? are you root?");
-	}
+  // todo: detach later
+  if(ptrace(PTRACE_ATTACH, pid, NULL, NULL)<0){
+    die("error attaching to a pid\n does the pid exist? are you root?");
+  }
 }
 
 void show_syscall(int syscall_no, int syscall_ret){
-	char* syscall_name = syscall_table[syscall_no];
-	char* return_prefix = "";
-	if(syscall_ret > 15){
-		return_prefix = "0x";
-	}
-	printf("%-15s = %s%x\n", syscall_name, return_prefix, syscall_ret);
+  char* syscall_name = syscall_table[syscall_no];
+  char* return_prefix = "";
+  if(syscall_ret > 15){
+    return_prefix = "0x";
+  }
+  printf("%-15s = %s%x\n", syscall_name, return_prefix, syscall_ret);
 }
 
 void tracer(int child_pid){
-	printf("I'm the tracer with pid=%d\n", getpid());
-	printf("child_status=%d\n", child_status);
-	wait(&child_status);
+  printf("I'm the tracer with pid=%d\n", getpid());
+  printf("child_status=%d\n", child_status);
+  wait(&child_status);
 
-	int syscall_count = 0;	
-	enum syscall_state syscall_state = PRE_SYSCALL;
-	struct user_regs_struct tracee_regs;
-	while(1){
+  int syscall_count = 0;
+  enum syscall_state syscall_state = PRE_SYSCALL;
+  struct user_regs_struct tracee_regs;
+  while(1){
 
-		if(ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL)<0){
-			die("error tracing syscalls");
-		}
-		wait(&child_status);
-		syscall_count += 1;
+    if(ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL)<0){
+      die("error tracing syscalls");
+    }
+    wait(&child_status);
+    syscall_count += 1;
 
-		if(WIFEXITED(child_status)){
-			printf("exited in %d syscalls with status=%d\n", syscall_count, child_status);
-			break;
-		}
+    if(WIFEXITED(child_status)){
+      printf("exited in %d syscalls with status=%d\n", syscall_count, child_status);
+      break;
+    }
 
-		if(ptrace(PTRACE_GETREGS, child_pid, NULL, &tracee_regs)<0){
-			die("error getting registers");
-		}
-		if(syscall_state == POST_SYSCALL){
-			int current_syscall_no = tracee_regs.orig_rax;
-			int current_syscall_return = tracee_regs.rax;
-			show_syscall(current_syscall_no, current_syscall_return);
-			syscall_state = PRE_SYSCALL;
-		}else{
-			syscall_state = POST_SYSCALL;
-		}
+    if(ptrace(PTRACE_GETREGS, child_pid, NULL, &tracee_regs)<0){
+      die("error getting registers");
+    }
+    if(syscall_state == POST_SYSCALL){
+      int current_syscall_no = tracee_regs.orig_rax;
+      int current_syscall_return = tracee_regs.rax;
+      show_syscall(current_syscall_no, current_syscall_return);
+      syscall_state = PRE_SYSCALL;
+    }else{
+      syscall_state = POST_SYSCALL;
+    }
 
-	}
+  }
 }
 
 int main(int argc, char* argv[]){
-	char* usage_banner = "usage: ./mystrace [<cmd>|-p <pid>]";
+  char* usage_banner = "usage: ./mystrace [<cmd>|-p <pid>]";
 
-	if(argc < 2){
-		die(usage_banner);
-	}
+  if(argc < 2){
+    die(usage_banner);
+  }
 
-	// really simple parsing
-	if(strcmp(argv[1], "-p")==0){
-		if(argc < 3){
-			die(usage_banner);
-		}
-		int pid = atoi(argv[2]);
-		tracee_with_pid(pid);
-		tracer(pid);
-	}else{
-		int pid = fork();
-		switch(pid){
-			case -1:
-				die("error forking");
-				break;
-			case 0:
-				tracee(argv+1);
-				break;
-			default: 
-				tracer(pid);
-				break;
-		}
-	}
-	return 0;
+  // really simple parsing
+  if(strcmp(argv[1], "-p")==0){
+    if(argc < 3){
+      die(usage_banner);
+    }
+    int pid = atoi(argv[2]);
+    tracee_with_pid(pid);
+    tracer(pid);
+  }else{
+    int pid = fork();
+    switch(pid){
+      case -1:
+        die("error forking");
+        break;
+      case 0:
+        tracee(argv+1);
+        break;
+      default:
+        tracer(pid);
+        break;
+    }
+  }
+  return 0;
 }
